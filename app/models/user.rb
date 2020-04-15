6409be8d6f0d306d4b17bc24a18b66c7a7c7b8bc
Cassy Myers <cassymyers@gmail.com>
2020-04-15 16:17:44 -0700

    add sign up features specs

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+    protected
+  def confirmation_required?
+    false
+  end
 end
