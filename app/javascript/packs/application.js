3e612823f66b0da892b7a7d8efdc477295324524
Cassy Myers <cassymyers@gmail.com>
2020-04-16 08:45:18 -0700

    Add a popup window to show a full post's content - Add bootstrap's modal component to show full post's content - Render the modal inside the home page's template - Add js to fill the modal with post's content and show it - Add CSS to style the modal

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,3 +1,6 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
 /* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
@@ -8,3 +11,4 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+
